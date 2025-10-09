d32e97a2ff59f46c4a6ec47379638a4b2bd8845e
Aryan Gupta <aryangupta02092002@gmail.com>
2025-10-09 12:26:30 +0530

    Seed data

---
+++ app/not-found.jsx
@@ -0,0 +1,16 @@
+import { Button } from '@/components/ui/button';
+import Link from 'next/link'
+ 
+export default function NotFound() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-[100vh] text-center">
+        <h1 className='text-6xl font-bold gradient-title mb-4'>404</h1>
+        <h2 className='text-2xl font-semibold mb-4'>Page Not Found</h2>
+        <Button asChild size="lg" className="bg-orange-500 hover:bg-orange-600">
+            <Link href="/">
+                Return Home
+            </Link>
+        </Button>
+    </div>
+  );
+}
